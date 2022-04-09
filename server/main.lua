39f23ccf97ab9273e17352ec46387bc836c6f3d9
Great-Liberty-RP <74937560+bamablood94@users.noreply.github.com>
2022-04-09 15:19:24 -0500

    testing new phone

--- server/main.lua
+++ server/main.lua
@@ -19,7 +19,7 @@ RegisterNetEvent('qb-busjob:server:NpcPay', function(Payment)
             local randomAmount = math.random(1, 5)
             local r1, r2 = math.random(1, 5), math.random(1, 5)
             if randomAmount == r1 or randomAmount == r2 then Payment = Payment + math.random(10, 20) end
-            Player.Functions.AddMoney('cash', Payment)
+            Player.Functions.AddMoney('cash', Payment, 'Payment')
         else
             DropPlayer(src, 'Attempting To Exploit')
         end
